efae827955897eff702bafc7dec7574bc9705489
Booomeii <604772159@qq.com>
2025-02-23 20:12:05 +0800

    Fix expandContractions prompt

--- lib/aiApi.ts
+++ lib/aiApi.ts
@@ -17,14 +17,14 @@ export async function expandContractions(text: string[], maxTokens = 300) {
       {
         role: "system",
         content:
-          'You are a helpful assistant that expands English contractions while ensuring grammatical correctness. When encountering possessive forms (e.g., George\'s), retain them as they are. Only expand standard contractions (e.g., "don\'t" to "do not"). Don\'t do anything else such as grammar correction or spell correction.',
+          'You are a helpful assistant that expands English contractions while ensuring grammatical correctness. When encountering possessive forms (e.g., George\'s), retain them as they are. Only expand standard contractions (e.g., "don\'t" to "do not"). Don\'t do anything else such as grammar correction or spell correction. Return Input if it is not a contraction.',
       },
       { role: "user", content: JSON.stringify(text) },
     ],
   });
 
   console.log("Input: ", text);
-  console.log("Output: ", JSON.parse(completion.choices[0].message.content!));
+  console.log("Output: ", completion.choices[0].message.content!);
 
   return JSON.parse(completion.choices[0].message.content!);
 }
