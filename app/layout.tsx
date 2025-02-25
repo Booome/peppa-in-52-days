f0fd91c13ad6252b6254d84f696a1519e5f17d64
Booomeii <604772159@qq.com>
2025-02-25 10:22:06 +0800

    Remove unused fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import { Header } from "@/components/Header";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { cn } from "@/lib/utils";
 import { ReduxProvider } from "@/redux/reduxProvider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Peppa in 52 Days",
@@ -31,12 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          `${geistSans.variable} ${geistMono.variable} antialiased`,
-          "flex min-h-screen w-full flex-col items-center bg-hero-bg bg-cover bg-center bg-no-repeat",
-        )}
-      >
+      <body className="flex min-h-screen w-full flex-col items-center bg-hero-bg bg-cover bg-center bg-no-repeat antialiased">
         <ReduxProvider>
           <TooltipProvider>
             <Header />
